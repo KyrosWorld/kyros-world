427db45464e90091800d203e622623f280972721
KyrosWorld <madanchandkhute@gmail.com>
2026-06-04 16:05:24 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 console.log("KYROS WORLD Loaded Successfully");
+
+const menuBtn = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+if(menuBtn){
+  menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+}
